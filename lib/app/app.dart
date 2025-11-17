5a2bbf7814f105acba5135f16eaea3a03472185e
alexandrakomkova <sashakomkova2001@gmail.com>
2025-11-17 04:47:04 -0800

    add multi bloc provider and repository provider

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:weather_app/data/location/location_repository_impl.dart';
 import 'package:weather_app/data/repository/weather_repository_impl.dart';
+import 'package:weather_app/presentation/cubit/location/location_cubit.dart';
 import 'package:weather_app/presentation/cubit/weather/weather_cubit.dart';
 
 import '../presentation/pages/weather_page.dart';
@@ -11,19 +12,29 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return RepositoryProvider(
-        create: (_) => WeatherRepositoryImpl(),
-        dispose: (repository) => repository.dispose(),
-        child: FutureBuilder(
-          future: _determinePosition(),
-          builder: (context, asyncSnapshot) {
-            return BlocProvider(
-              create: (context) =>
-                  WeatherCubit(context.read<WeatherRepositoryImpl>()),
-              child: const AppView(),
-            );
-          }
-        )
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider(
+          create: (_) => WeatherRepositoryImpl(),
+          dispose: (repository) => repository.dispose(),
+        ),
+        RepositoryProvider(
+          create: (context) => LocationRepositoryImpl(),
+        ),
+      ],
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider<LocationCubit>(
+            create: (context) =>
+                LocationCubit(context.read<LocationRepositoryImpl>())..getPosition(),
+          ),
+          BlocProvider<WeatherCubit>(
+            create: (context) =>
+                WeatherCubit(context.read<WeatherRepositoryImpl>()),
+          ),
+        ],
+        child: const AppView(),
+      ),
     );
   }
 }
@@ -34,51 +45,11 @@ class AppView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: WeatherPage(),
+      debugShowCheckedModeBanner: false,
+      home: WeatherPage(),
     );
   }
 }
-
-
-Future<Position> _determinePosition() async {
-  bool serviceEnabled;
-  LocationPermission permission;
-
-  // Test if location services are enabled.
-  serviceEnabled = await Geolocator.isLocationServiceEnabled();
-  if (!serviceEnabled) {
-    // Location services are not enabled don't continue
-    // accessing the position and request users of the
-    // App to enable the location services.
-    return Future.error('Location services are disabled.');
-  }
-
-  permission = await Geolocator.checkPermission();
-  if (permission == LocationPermission.denied) {
-    permission = await Geolocator.requestPermission();
-    if (permission == LocationPermission.denied) {
-      // Permissions are denied, next time you could try
-      // requesting permissions again (this is also where
-      // Android's shouldShowRequestPermissionRationale
-      // returned true. According to Android guidelines
-      // your App should show an explanatory UI now.
-      return Future.error('Location permissions are denied');
-    }
-  }
-
-  if (permission == LocationPermission.deniedForever) {
-    // Permissions are denied forever, handle appropriately.
-    return Future.error(
-        'Location permissions are permanently denied, we cannot request permissions.');
-  }
-
-  // When we reach here, permissions are granted and we can
-  // continue accessing the position of the device.
-  return await Geolocator.getCurrentPosition();
-}
-
-
 
 
 
