fc00307a02e1a9eeb4b58f46a4286c8f97af5c77
alexandrakomkova <sashakomkova2001@gmail.com>
2025-11-18 04:20:57 -0800

    add retry button

--- lib/presentation/widgets/weather_error.dart
+++ lib/presentation/widgets/weather_error.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../cubit/weather/weather_cubit.dart';
 
 class WeatherError extends StatelessWidget {
   const WeatherError({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text('Something went wrong!'),
-      ],
+    return Padding(
+        padding: const EdgeInsets.fromLTRB(30, 1.5 * kToolbarHeight, 30, 20),
+        child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child:
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                    'Something went wrong! Please, try later.',
+                  style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300
+                  ),
+                ),
+                SizedBox(height: 10.0,),
+                OutlinedButton(
+                    onPressed: () {
+                      context.read<WeatherCubit>().refreshWeather();
+                    },
+                    child: Text(
+                        'Try again',
+                    ),
+                )
+              ],
+            )
+        )
     );
   }
 }
